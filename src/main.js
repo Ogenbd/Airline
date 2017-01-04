2bde0da872a2a4dc808a6ef1833e4378a32f34dd
theaarontaylor <aarontaylor613@gmail.com>
2017-01-04 10:17:01 +0200

    flight component

--- src/main.js
+++ src/main.js
@@ -2,13 +2,16 @@ import style from './style.scss';
 import Vue from 'vue'
 import VueRouter from 'vue-router';
 import VueResource from 'vue-resource'
+import VeeValidate from 'vee-validate';
 
 import Home from './components/home/home.vue';
 import MainNav from './components/main-nav/main-nav.vue';
 import Plane from './components/plane/plane.vue'
+import Flight from './components/flight/flight.vue'
 
 Vue.use(VueRouter);
 Vue.use(VueResource)
+Vue.use(VeeValidate);
 
 Vue.http.options.root = 'http://localhost/airline/api'
 
@@ -18,6 +21,9 @@ const routes = [{
 },{
   path: '/plane',
   component: Plane
+},{
+  path: '/flight',
+  component: Flight
 }]
 
 const router = new VueRouter({
